c974e59eaec63c9ee7181b131a737efb5da8c0b7
vsr <vsr@pop-os.localdomain>
2023-02-01 18:44:41 -0300

    Adding React Navigation

--- App.jsx
+++ App.jsx
@@ -1,7 +1,10 @@
+import { NavigationContainer } from '@react-navigation/native';
 import { Login } from './src/screens/Login';
 
 export default function App() {
   return (
-    <Login />
+    <NavigationContainer>
+      <Login />
+    </NavigationContainer>
   );
 }
